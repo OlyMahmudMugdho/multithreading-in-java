59379e3124a2185ba14c6dcf505049bac3bc184a
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-11-24 12:30:25 +0600

    set thread name using setName() method

--- ThreadMethods/src/main/java/org/mahmud/Main.java
+++ ThreadMethods/src/main/java/org/mahmud/Main.java
@@ -3,8 +3,9 @@ package org.mahmud;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         Worker worker = new Worker();
+        worker.setName("Worker Thread");
         worker.start();
-        worker.join();
+        worker.join(); // wait for the 'worker' thread from main thread
         System.out.println("main thread : main thread execution finished");
     }
 }
